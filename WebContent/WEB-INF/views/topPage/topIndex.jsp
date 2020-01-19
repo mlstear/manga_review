0c652f1194658758a56960a0c402d04659b3901b
reo.murata <lavi.mlty@gmail.com>
2020-01-19 15:52:59 +0900

    Add Review index

--- WebContent/WEB-INF/views/topPage/topIndex.jsp
+++ WebContent/WEB-INF/views/topPage/topIndex.jsp
@@ -7,7 +7,40 @@
             <c:out value="${flush}"></c:out>
          </div>
     </c:if>
-    <h2>漫画レビューサイトへようこそ</h2>
+    <h2>最新レビュー</h2>
     <h4>レビューを投稿したい作品が無い場合は「漫画タイトル一覧」より作品を新規追加してください。</h4><br />
+    <table id="review_list">
+       <tbody>
+          <tr>
+             <th class="review_comicTitle">作品タイトル</th>
+             <th class="review_Title">レビュータイトル</th>
+             <th class="review_poster">投稿者</th>
+             <th class="review_view">詳細</th>
+             <th class="review_score">スコア</th>
+          </tr>
+          <c:forEach var="review" items="${reviews}" varStatus="status">
+             <tr class="row${status.count%2}">
+                <td class="review_comicTitle"><c:out value="${review.comic.title}"/></td>
+                <td Class="review_Title">${review.title}</td>
+                <td class="review_poster"><c:out value="${review.poster.name}"/></td>
+                <td class="review_view"><a href="<c:url value='/reviews/show?id=${review.id}' />">レビューを見る</a></td>
+                <td Class="review_Score">${review.score}</td>
+             </tr>
+          </c:forEach>
+       </tbody>
+    </table>
+     <div id="pagination">
+            （全 ${reviews_count} 件）<br />
+            <c:forEach var="i" begin="1" end="${((reviews_count - 1) / 15) + 1}" step="1">
+                <c:choose>
+                    <c:when test="${i == page}">
+                        <c:out value="${i}" />&nbsp;
+                    </c:when>
+                    <c:otherwise>
+                        <a href="<c:url value='/index.html?page=${i}' />"><c:out value="${i}" /></a>&nbsp;
+                    </c:otherwise>
+                </c:choose>
+            </c:forEach>
+       </div>
   </c:param>
 </c:import>
--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -1,13 +1,18 @@
 package controllers.toppage;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Review;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class TopPageIndexServlet
@@ -28,6 +33,28 @@ public class TopPageIndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        int page;
+        try{
+            page=Integer.parseInt(request.getParameter("page"));
+        }catch(Exception e){
+            page=1;
+        }
+        List<Review> reviews=em.createNamedQuery("getAllReviews",Review.class)
+                               .setFirstResult(15 * (page - 1))
+                               .setMaxResults(15)
+                               .getResultList();
+        long reviews_count = (long)em.createNamedQuery("getReviewsCount", Long.class)
+                .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("reviews", reviews);
+        request.setAttribute("reviews_count", reviews_count);
+        request.setAttribute("page", page);
+
+
         if(request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
             request.getSession().removeAttribute("flush");
